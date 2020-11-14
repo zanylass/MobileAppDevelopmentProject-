e1f3582493c4c1ab1a62a51b9284b8d83351b931
ozar98 <ozar007@gmail.com>
2020-11-14 23:47:22 +0500

    Update QuizDbHelperRate.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/QuizDbHelperRate.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/QuizDbHelperRate.java
@@ -37,17 +37,21 @@ public class QuizDbHelperRate extends SQLiteOpenHelper {
         onCreate(db);
     }
     private void fillQuestionsTable() {
-        Question q1 = new Question("?", "A: 12 for every 7", "B: 7 for every 12", "C: 7 for every 19","D: 19 for every 7", 1);
+        Question q1 = new Question("Vivi is a drummer for a band. She burns 756 calories while drumming for 3 hours. She burns the same number of calories each hour.\n How many calories does Vivi burn per hour?", "A: 249", "B: 252", "C: 253","D: 256", 2);
         addQuestion(q1);
-        Question q2 = new Question("1:2?", "A: 3:7", "B: 4:7", "C: 7:4","D: 3:12", 1);
+        Question q2 = new Question("Luke can paint 91 portraits in 7 weeks.\nHow many portraits can Luke paint in 4 weeks?", "A: 52", "B: 36", "C: 66","D: 42", 1);
         addQuestion(q2);
-        Question q3 = new Question("1;3?", "A: 4 to 3", "B: 3 to 4", "C: 4 to 9","D: 4 to 12", 3);
+        Question q3 = new Question("A box of 15 cookies costs $9.\nWhat is the cost for 1 cookie?", "A: 0.55", "B: 0.68", "C: 0.60","D: 0.62", 3);
         addQuestion(q3);
-        Question q4 = new Question("1;5?", "A: 14:6", "B: 3:24", "C: 14:3","D: 24:3", 3);
+        Question q4 = new Question("Julio bikes 5 miles in 20 minutes on the Walkway over the Hudson.\nHow many miles per minute does Julio bike?", "A: 2.5 miles per minute", "B: 0.25 miles per minute", "C: 0.25 miles per hour","D: 2.5 miles per hour", 2);
         addQuestion(q4);
-        Question q5 = new Question("123?", "A: 6 for every 9", "B: 6 for every 13", "C: 6 for every 30","D: 13 for every 6", 2);
+        Question q5 = new Question(" Movie ticket sales are as follows:\n" +
+                "Adult: 20 tickets for $250\n" +
+                "Child: 15 tickets for $71.25\n" +
+                "Senior: 5 tickets for $37.50\n" +
+                "How much does one (1) SENIOR ticket cost?", "A: $7.00", "B: $0.13", "C: $32.50","D: $7.50", 4);
         addQuestion(q5);
-        Question q6 = new Question("1;7?", "A: 3 to 8", "B: 3 to 10", "C: 8 to 3","D: 3 to 18", 2);
+        Question q6 = new Question("A store sees 120 customers in 12 hours.\nWhat is the unit rate (in customers per hour)?", "A: 6 customers per hour", "B: 10 customers per hour", "C: 12 customers per hour","D: 20 customers per hour", 2);
         addQuestion(q6);
     }
     private void addQuestion(Question question) {
