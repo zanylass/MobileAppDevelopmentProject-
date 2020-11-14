030a334db578d0fe6b40228ce46a8e639f4928f1
ozar98 <ozar007@gmail.com>
2020-11-14 23:17:40 +0500

    Update MathArithOperationContentPage.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathArithOperationContentPage.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathArithOperationContentPage.java
@@ -26,60 +26,60 @@ public class MathArithOperationContentPage extends AppCompatActivity {
         ar1Button=(Button)findViewById(R.id.buttonAr1);
         ar2Button=(Button)findViewById(R.id.buttonAr1);
         ar3Button=(Button)findViewById(R.id.buttonAr1);
-//        ar1Button.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v){
-//                openAr1Quiz();
-//            }
-//        });
-//        ar2Button.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v){
-//                openAr1Quiz();
-//            }
-//        });
-//        ar3Button.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v){
-//                openAr1Quiz();
-//            }
-//        });
-//        arOper1.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v){
-//                openAr1Topic();
-//            }
-//        });
-//        arOper2.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v){
-//                openAr1Topic();
-//            }
-//        });
-//        arOper3.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v){
-//                openAr1Topic();
-//            }
-//        });
+        ar1Button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openAr1Quiz();
+            }
+        });
+        ar2Button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openAr1Quiz();
+            }
+        });
+        ar3Button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openAr1Quiz();
+            }
+        });
+        arOper1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openAr1Topic();
+            }
+        });
+        arOper2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openAr1Topic();
+            }
+        });
+        arOper3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                openAr1Topic();
+            }
+        });
     }
-    //public void openAr1Topic(){
-        //Intent intent = new Intent(this, RatioContentPage.class);
-        //startActivity(intent);
-        // }
-    //public void openAr2Topic(){
-        //Intent intent = new Intent(this, RatioContentPage.class);
-        //startActivity(intent);
-        // }
-    //public void openAr3Topic(){
-        //Intent intent = new Intent(this, RatioContentPage.class);
-        //startActivity(intent);
-        // }
-    //public void openAr1Quiz(){
-    //  Intent intent = new Intent(this, RatioContentPage.class);
-    //  startActivity(intent);
-    // }
-    //public void openAr2Quiz(){
-    //  Intent intent = new Intent(this, RatioContentPage.class);
-    //  startActivity(intent);
-    // }
-    //public void openAr3Quiz(){
-    //  Intent intent = new Intent(this, RatioContentPage.class);
-    //  startActivity(intent);
-    // }
+    public void openAr1Topic(){
+        Intent intent = new Intent(this, MathAR1.class);
+        startActivity(intent);
+         }
+    public void openAr2Topic(){
+        Intent intent = new Intent(this, MathAR2.class);
+        startActivity(intent);
+         }
+    public void openAr3Topic(){
+        Intent intent = new Intent(this, MathAR3.class);
+        startActivity(intent);
+         }
+    public void openAr1Quiz(){
+      Intent intent = new Intent(this, MathARQuiz1.class);
+      startActivity(intent);
+     }
+    public void openAr2Quiz(){
+      Intent intent = new Intent(this, MathARQuiz2.class);
+      startActivity(intent);
+     }
+    public void openAr3Quiz(){
+      Intent intent = new Intent(this, MathARQuiz3.class);
+      startActivity(intent);
+     }
 
 }
