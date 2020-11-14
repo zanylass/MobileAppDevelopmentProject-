b251dd9f123b26495df9ad1e9efb7103928468b7
ozar98 <ozar007@gmail.com>
2020-11-14 21:17:23 +0500

    Update RatioContentPage.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/RatioContentPage.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/RatioContentPage.java
@@ -18,14 +18,6 @@ public class RatioContentPage extends AppCompatActivity {
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
     }
 }
