3f761ac67f38d31faa865f043f85c0f6e23f1533
ozar98 <ozar007@gmail.com>
2020-11-15 13:19:37 +0500

    Update QuizDbHelperAR3.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/QuizDbHelperAR3.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/QuizDbHelperAR3.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import java.util.ArrayList;
 
 public class QuizDbHelperAR3 extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "MothAR3Quiz.db";
+    private static final String DATABASE_NAME = "MathAR3Quiz.db";
     private static final int DATABASE_VERSION = 1;
     private SQLiteDatabase db;
     public QuizDbHelperAR3(Context context) {
@@ -37,17 +37,17 @@ public class QuizDbHelperAR3 extends SQLiteOpenHelper {
         onCreate(db);
     }
     private void fillQuestionsTable() {
-        Question q1 = new Question("?", "A: 12 for every 7", "B: 7 for every 12", "C: 7 for every 19","D: 19 for every 7", 1);
+        Question q1 = new Question("Shelley is applying the order of operations to the following expression. 3−16÷2⋅5⋅3\nWhat is the first step Shelley should use to solve the problem? ?", "A: 3-16", "B: 16÷2", "C: 2⋅5","D: 5⋅3", 2);
         addQuestion(q1);
-        Question q2 = new Question("?", "A: 3:7", "B: 4:7", "C: 7:4","D: 3:12", 1);
+        Question q2 = new Question("Add, subtract, multiply and divide (+, −, ×, ÷) are all examples of?", "A: divisors", "B: exponents", "C: operators","D: roots", 3);
         addQuestion(q2);
-        Question q3 = new Question("?", "A: 4 to 3", "B: 3 to 4", "C: 4 to 9","D: 4 to 12", 3);
+        Question q3 = new Question("Evaluate the expression. Use the order of operations.\n100÷25÷2?", "A: 2", "B: 25", "C: 0","D: 8", 1);
         addQuestion(q3);
-        Question q4 = new Question("?", "A: 14:6", "B: 3:24", "C: 14:3","D: 24:3", 3);
+        Question q4 = new Question("Which operation should you do first when you simplify 85 + (25 x 5)?", "A: addition", "B: subtraction", "C: multiplication","D: division", 3);
         addQuestion(q4);
-        Question q5 = new Question("?", "A: 6 for every 9", "B: 6 for every 13", "C: 6 for every 30","D: 13 for every 6", 2);
+        Question q5 = new Question("Which of the following is NOT equivalent to 120?", "A: 12 (8 + 2)", "B: 6 x 6 + 18 x 2", "C: 6 x 18 + 6 x 2","D: 6(18 + 2)", 2);
         addQuestion(q5);
-        Question q6 = new Question("?", "A: 3 to 8", "B: 3 to 10", "C: 8 to 3","D: 3 to 18", 2);
+        Question q6 = new Question("What is the acronym for the order of operations?", "A: DASEFX", "B: SINCOS", "C: PDIDDY","D: PEMDAS", 4);
         addQuestion(q6);
     }
     private void addQuestion(Question question) {
