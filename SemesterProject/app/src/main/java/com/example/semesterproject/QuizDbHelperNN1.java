28b430ede0b029aad7709a7b4e6685c650c17c7e
ozar98 <ozar007@gmail.com>
2020-11-15 14:24:01 +0500

    Update QuizDbHelperNN1.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/QuizDbHelperNN1.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/QuizDbHelperNN1.java
@@ -37,17 +37,17 @@ public class QuizDbHelperNN1 extends SQLiteOpenHelper {
         onCreate(db);
     }
     private void fillQuestionsTable() {
-        Question q1 = new Question("2.888+42=?", "A: 44.888", "B: 43.888", "C: 44.8","D: 44.88", 1);
+        Question q1 = new Question("The temperature at 3:00 PM was 15 degrees. At midnight the temperature was -2 degrees. What's the difference between those two temperatures?", "A: 13 degrees", "B: 17 degrees", "C: 19 degrees","D: 10 degrees", 2);
         addQuestion(q1);
-        Question q2 = new Question("To visit his grandmother, Luis takes a train 6.37 kilometers and a car 5.45 kilometers.\nHow many kilometers is Luis's journey in total?", "A: 13.86", "B: 11.82", "C: 12.88","D: 10.54", 2);
+        Question q2 = new Question("Which number is the greatest?", "A: 20", "B: -100", "C: -45","D: 50", 4);
         addQuestion(q2);
-        Question q3 = new Question("Cayden has several screws on a scale, and the scale reads 80.955 g. Cayden adds 1 more screw, and the scale reads 84.81 g.\nWhat is the mass of the last screw Cayden adds?", "A: 3.855g", "B: 4.657g", "C: 2.845g","D: 3.085g", 1);
+        Question q3 = new Question("Integers that are less than zero are called ___________ numbers.", "A: positive", "B: negative", "C: equal","D: complex", 2);
         addQuestion(q3);
-        Question q4 = new Question("A relay race covers 3/2 kilometers, and each runner on a team will run 1/4 of a kilometer.\nHow many runners are needed for a team?", "A: 8 runners", "B: 4 runners", "C: 6 runners","D: 9 runners", 3);
+        Question q4 = new Question("Mr. Lovell was playing cards with his son. In the first round he earned 52 points. In the second round he lost 20 points. How would you write this as an addition problem?", "A: 52 + 20", "B: -52 + -20", "C: 52 + -20","D: -52 + 20", 3);
         addQuestion(q4);
-        Question q5 = new Question("A piano 9/2 hours available to teach in a night. Each lesson will last 3/2 hours.\n How many lessons can the teacher schedule in a night?", "A: 6 lessons", "B: 5 lessons", "C: 9 lessons","D: 3 lessons", 4);
+        Question q5 = new Question("If the temperature in Minnesota drops 35 degrees Fahrenheit it will be -10 degrees Fahrenheit. What is the current temperature in Minnesota?", "A: 45", "B: 25", "C: -10","D: 10", 2);
         addQuestion(q5);
-        Question q6 = new Question("A baker has 21/4 pieces in her shop. She cut the pies in pieces that are each 1/8 of a whole pie. How many pieces of pie does she have?", "A: 36 pieces", "B: 48 pieces", "C: 42 pieces","D: 40 pieces", 3);
+        Question q6 = new Question("The town of Apple Valley, California is at an elevation of 2,900 feet above sea level. Death Valley, California has an elevation of 282 feet below sea level. What is the difference in elevation of these two towns?", "A: 3,182", "B: 2,616", "C: -2,616","D: -3,182", 1);
         addQuestion(q6);
     }
     private void addQuestion(Question question) {
