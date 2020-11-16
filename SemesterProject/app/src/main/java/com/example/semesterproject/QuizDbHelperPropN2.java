344ecfe271f0e43369108a70f712538f3f7b577e
ozar98 <ozar007@gmail.com>
2020-11-16 17:26:48 +0500

    Update QuizDbHelperPropN2.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/QuizDbHelperPropN2.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/QuizDbHelperPropN2.java
@@ -37,17 +37,17 @@ public class QuizDbHelperPropN2 extends SQLiteOpenHelper {
         onCreate(db);
     }
     private void fillQuestionsTable() {
-        Question q1 = new Question("The temperature at 3:00 PM was 15 degrees. At midnight the temperature was -2 degrees. What's the difference between those two temperatures?", "A: 13 degrees", "B: 17 degrees", "C: 19 degrees","D: 10 degrees", 2);
+        Question q1 = new Question("The smallest whole number is _________.", "A: 1", "B: 0", "C: 2","D: None of those",4 );
         addQuestion(q1);
-        Question q2 = new Question("Which number is the greatest?", "A: 20", "B: -100", "C: -45","D: 50", 4);
+        Question q2 = new Question("The whole number which has no predecessor is ____.", "A: 0", "B: 2", "C: None of those","D: 1",3 );
         addQuestion(q2);
-        Question q3 = new Question("Integers that are less than zero are called ___________ numbers.", "A: positive", "B: negative", "C: equal","D: complex", 2);
+        Question q3 = new Question("Fill in the blanks\n18 × 47 = 18 × 50 - 18 × ________?", "A: 0", "B: 3", "C: 1","D: 2", 2);
         addQuestion(q3);
-        Question q4 = new Question("Mr. Lovell was playing cards with his son. In the first round he earned 52 points. In the second round he lost 20 points. How would you write this as an addition problem?", "A: 52 + 20", "B: -52 + -20", "C: 52 + -20","D: -52 + 20", 3);
+        Question q4 = new Question("The value of 300 × 4 × 0 × 10 is ____", "A: 0", "B: 1200", "C: 120000","D: 12000", 1);
         addQuestion(q4);
-        Question q5 = new Question("If the temperature in Minnesota drops 35 degrees Fahrenheit it will be -10 degrees Fahrenheit. What is the current temperature in Minnesota?", "A: 45", "B: 25", "C: -10","D: 10", 2);
+        Question q5 = new Question("?", "A: ", "B: ", "C: ","D: ", );
         addQuestion(q5);
-        Question q6 = new Question("The town of Apple Valley, California is at an elevation of 2,900 feet above sea level. Death Valley, California has an elevation of 282 feet below sea level. What is the difference in elevation of these two towns?", "A: 3,182", "B: 2,616", "C: -2,616","D: -3,182", 1);
+        Question q6 = new Question("?", "A: ", "B: ", "C: ","D: ", );
         addQuestion(q6);
     }
     private void addQuestion(Question question) {
