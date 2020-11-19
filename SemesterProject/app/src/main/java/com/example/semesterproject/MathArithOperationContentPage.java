e568c2122d012651c455fb76e516f8ea3fdc6a94
ozar98 <ozar007@gmail.com>
2020-11-20 01:01:33 +0500

    Update MathArithOperationContentPage.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathArithOperationContentPage.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathArithOperationContentPage.java
@@ -2,8 +2,13 @@ package com.example.semesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PowerManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +20,37 @@ public class MathArithOperationContentPage extends AppCompatActivity {
     private Button ar1Button;
     private Button ar2Button;
     private Button ar3Button;
+
+    //
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    private ServiceConnection Scon =new ServiceConnection(){
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder)binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService(){
+        bindService(new Intent(this,MusicService.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService()
+    {
+        if(mIsBound)
+        {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +92,30 @@ public class MathArithOperationContentPage extends AppCompatActivity {
                 openAr3Topic();
             }
         });
+        //
+        //
+        doBindService();
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        startService(music);
+        HomeWatcher mHomeWatcher;
+
+        mHomeWatcher = new HomeWatcher(this);
+        mHomeWatcher.setOnHomePressedListener(new HomeWatcher.OnHomePressedListener() {
+            @Override
+            public void onHomePressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+            @Override
+            public void onHomeLongPressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+        });
+        mHomeWatcher.startWatch();
     }
     public void openAr1Topic(){
         Intent intent = new Intent(this, MathAR1.class);
@@ -81,5 +141,45 @@ public class MathArithOperationContentPage extends AppCompatActivity {
       Intent intent = new Intent(this, MathARQuiz3.class);
       startActivity(intent);
      }
+
+     //music
+     //
+     @Override
+     protected void onDestroy() {
+         super.onDestroy();
+
+
+         doUnbindService();
+         Intent music = new Intent();
+         music.setClass(this,MusicService.class);
+         stopService(music);
+     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mServ != null) {
+            mServ.resumeMusic();
+        }
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        PowerManager pm = (PowerManager)
+                getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = false;
+        if (pm != null) {
+            isScreenOn = pm.isScreenOn();
+        }
+
+        if (!isScreenOn) {
+            if (mServ != null) {
+                mServ.pauseMusic();
+            }
+        }
+
+    }
 
 }
