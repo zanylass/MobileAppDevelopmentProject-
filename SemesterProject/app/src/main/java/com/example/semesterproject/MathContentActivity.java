fd892dc0eacd2642955cc7bfbfebe450d38c1367
ozar98 <ozar007@gmail.com>
2020-11-20 01:01:37 +0500

    Update MathContentActivity.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathContentActivity.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathContentActivity.java
@@ -2,8 +2,13 @@ package com.example.semesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PowerManager;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,7 +18,36 @@ public class MathContentActivity extends AppCompatActivity {
     public TextView negNumbers;
     public TextView propNumbers;
 
+    //
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    private ServiceConnection Scon =new ServiceConnection(){
 
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder)binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService(){
+        bindService(new Intent(this,MusicService.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService()
+    {
+        if(mIsBound)
+        {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +81,31 @@ public class MathContentActivity extends AppCompatActivity {
             }
         });
 
+        //
+        //
+        doBindService();
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        startService(music);
+        HomeWatcher mHomeWatcher;
+
+        mHomeWatcher = new HomeWatcher(this);
+        mHomeWatcher.setOnHomePressedListener(new HomeWatcher.OnHomePressedListener() {
+            @Override
+            public void onHomePressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+            @Override
+            public void onHomeLongPressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+        });
+        mHomeWatcher.startWatch();
+
 
 
     }
@@ -65,7 +124,48 @@ public class MathContentActivity extends AppCompatActivity {
     }
     public void openMathPropNumbers() {
         Intent intent = new Intent(this, MathPropNumContentPage.class);
-        startActivity(intent);}
+        startActivity(intent);
+    }
+    //
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+        doUnbindService();
+        Intent music = new Intent();
+        music.setClass(this,MusicService.class);
+        stopService(music);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mServ != null) {
+            mServ.resumeMusic();
+        }
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        PowerManager pm = (PowerManager)
+                getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = false;
+        if (pm != null) {
+            isScreenOn = pm.isScreenOn();
+        }
+
+        if (!isScreenOn) {
+            if (mServ != null) {
+                mServ.pauseMusic();
+            }
+        }
+
+    }
+
+
 
 
 }
