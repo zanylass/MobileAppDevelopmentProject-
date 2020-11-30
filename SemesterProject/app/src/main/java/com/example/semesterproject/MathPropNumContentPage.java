6b365df4e377b736e4c8b0ed5912c1040e93fa15
zanylass <57338344+zanylass@users.noreply.github.com>
2020-11-30 22:41:34 +0600

    Update MathPropNumContentPage.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathPropNumContentPage.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathPropNumContentPage.java
@@ -2,10 +2,17 @@ package com.example.semesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PowerManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -20,6 +27,37 @@ public class MathPropNumContentPage extends AppCompatActivity {
     private Button pn3quiz;
     private TextView pntopicx;
 
+
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    private ServiceConnection Scon =new ServiceConnection(){
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder)binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService(){
+        bindService(new Intent(this,MusicService.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService()
+    {
+        if(mIsBound)
+        {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+    //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +70,8 @@ public class MathPropNumContentPage extends AppCompatActivity {
         pn2quiz=(Button) findViewById(R.id.buttonPN2);
         pn3quiz=(Button) findViewById(R.id.buttonPN3);
         lcm=(TextView)findViewById(R.id.LCM);
+
+
         lcm.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 openPN4topic();
@@ -72,6 +112,33 @@ public class MathPropNumContentPage extends AppCompatActivity {
                 openPN3quiz();
             }
         });
+
+        //
+        doBindService();
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        startService(music);
+        HomeWatcher mHomeWatcher;
+
+        mHomeWatcher = new HomeWatcher(this);
+        mHomeWatcher.setOnHomePressedListener(new HomeWatcher.OnHomePressedListener() {
+            @Override
+            public void onHomePressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+            @Override
+            public void onHomeLongPressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+        });
+        mHomeWatcher.startWatch();
+
+
+
     }
     public void openPN1topic(){
         Intent intent = new Intent(this, MathPN1.class);
@@ -104,5 +171,45 @@ public class MathPropNumContentPage extends AppCompatActivity {
     public void openPN3quiz(){
         Intent intent = new Intent(this, MathPropN3Quiz.class);
         startActivity(intent);
+    }
+
+    //
+    //
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+        doUnbindService();
+        Intent music = new Intent();
+        music.setClass(this,MusicService.class);
+        stopService(music);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mServ != null) {
+            mServ.resumeMusic();
+        }
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        PowerManager pm = (PowerManager)
+                getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = false;
+        if (pm != null) {
+            isScreenOn = pm.isScreenOn();
+        }
+
+        if (!isScreenOn) {
+            if (mServ != null) {
+                mServ.pauseMusic();
+            }
+        }
+
     }
 }
