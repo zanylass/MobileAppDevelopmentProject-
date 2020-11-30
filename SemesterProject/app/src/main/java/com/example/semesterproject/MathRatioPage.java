b950818fd379d214abc36d6a3e55233c532a7abb
zanylass <57338344+zanylass@users.noreply.github.com>
2020-11-30 22:41:40 +0600

    Update MathRatioPage.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathRatioPage.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathRatioPage.java
@@ -3,9 +3,14 @@ package com.example.semesterproject;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PowerManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,6 +29,37 @@ public class MathRatioPage extends AppCompatActivity {
     public Button quiz1;
     public Button quiz2;
     public Button quiz3;
+
+    //
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    private ServiceConnection Scon =new ServiceConnection(){
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder)binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService(){
+        bindService(new Intent(this,MusicService.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService()
+    {
+        if(mIsBound)
+        {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+    //
 
 
     @Override
@@ -71,6 +107,30 @@ public class MathRatioPage extends AppCompatActivity {
                 openPercentageTopic();
             }
         });
+
+        //
+        doBindService();
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        startService(music);
+        HomeWatcher mHomeWatcher;
+
+        mHomeWatcher = new HomeWatcher(this);
+        mHomeWatcher.setOnHomePressedListener(new HomeWatcher.OnHomePressedListener() {
+            @Override
+            public void onHomePressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+            @Override
+            public void onHomeLongPressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+        });
+        mHomeWatcher.startWatch();
     }
 
     public void openRatioTopic(){
@@ -130,5 +190,44 @@ public class MathRatioPage extends AppCompatActivity {
     public void openQuiz3() {
         Intent intent = new Intent(this, MathQuizPercentage.class);
         startActivity(intent);
+    }
+
+    //
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+        doUnbindService();
+        Intent music = new Intent();
+        music.setClass(this,MusicService.class);
+        stopService(music);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mServ != null) {
+            mServ.resumeMusic();
+        }
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        PowerManager pm = (PowerManager)
+                getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = false;
+        if (pm != null) {
+            isScreenOn = pm.isScreenOn();
+        }
+
+        if (!isScreenOn) {
+            if (mServ != null) {
+                mServ.pauseMusic();
+            }
+        }
+
     }
 }
