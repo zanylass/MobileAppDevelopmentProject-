3fc2a99ab6a3ff314bab5ed84d0b6c7203b575b4
zanylass <57338344+zanylass@users.noreply.github.com>
2020-11-30 22:41:30 +0600

    Update MathNegNumContentPage.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathNegNumContentPage.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathNegNumContentPage.java
@@ -2,8 +2,13 @@ package com.example.semesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PowerManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,6 +21,37 @@ public class MathNegNumContentPage extends AppCompatActivity {
     private Button nn1Button;
     private Button nn2Button;
     private Button nn3Button;
+
+    //
+    private boolean mIsBound = false;
+    private MusicService mServ;
+    private ServiceConnection Scon =new ServiceConnection(){
+
+        public void onServiceConnected(ComponentName name, IBinder
+                binder) {
+            mServ = ((MusicService.ServiceBinder)binder).getService();
+        }
+
+        public void onServiceDisconnected(ComponentName name) {
+            mServ = null;
+        }
+    };
+
+    void doBindService(){
+        bindService(new Intent(this,MusicService.class),
+                Scon, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+    }
+
+    void doUnbindService()
+    {
+        if(mIsBound)
+        {
+            unbindService(Scon);
+            mIsBound = false;
+        }
+    }
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +93,31 @@ public class MathNegNumContentPage extends AppCompatActivity {
                 openNN2Topic();
             }
         });
+
+        //
+        //
+        doBindService();
+        Intent music = new Intent();
+        music.setClass(this, MusicService.class);
+        startService(music);
+        HomeWatcher mHomeWatcher;
+
+        mHomeWatcher = new HomeWatcher(this);
+        mHomeWatcher.setOnHomePressedListener(new HomeWatcher.OnHomePressedListener() {
+            @Override
+            public void onHomePressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+            @Override
+            public void onHomeLongPressed() {
+                if (mServ != null) {
+                    mServ.pauseMusic();
+                }
+            }
+        });
+        mHomeWatcher.startWatch();
     }
     public void openNN1Topic(){
         Intent intent = new Intent(this, MathNN1.class);
@@ -82,6 +143,45 @@ public class MathNegNumContentPage extends AppCompatActivity {
     public void openNN3Quiz(){
         Intent intent = new Intent(this, MathNNQuiz3.class);
         startActivity(intent);
+    }
+
+    //
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+        doUnbindService();
+        Intent music = new Intent();
+        music.setClass(this,MusicService.class);
+        stopService(music);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mServ != null) {
+            mServ.resumeMusic();
+        }
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        PowerManager pm = (PowerManager)
+                getSystemService(Context.POWER_SERVICE);
+        boolean isScreenOn = false;
+        if (pm != null) {
+            isScreenOn = pm.isScreenOn();
+        }
+
+        if (!isScreenOn) {
+            if (mServ != null) {
+                mServ.pauseMusic();
+            }
+        }
+
     }
 
 
