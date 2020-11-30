526378f2073f92b47a1a586ba3510034a7474bfc
zanylass <57338344+zanylass@users.noreply.github.com>
2020-11-30 22:37:53 +0600

    Update MainActivity.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MainActivity.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MainActivity.java
@@ -9,20 +9,35 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     public Button mathButton;
+    public Button scienceButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mathButton=(Button)findViewById(R.id.MathButton);
+        scienceButton = (Button) findViewById(R.id.ScienceButton);
+
         mathButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openMathContent();
             }
         });
+
+        scienceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openScienceContent();
+            }
+        });
     }
     public void openMathContent() {
         Intent intent = new Intent(this, MathContentActivity.class);
+        startActivity(intent);
+    }
+
+    public void openScienceContent(){
+        Intent intent = new Intent(this, activity_science_content.class);
         startActivity(intent);
     }
 }
