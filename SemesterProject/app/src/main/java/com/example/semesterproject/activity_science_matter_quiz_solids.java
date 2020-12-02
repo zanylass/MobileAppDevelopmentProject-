4023794242cf05e140d27acbc003c1f3161f6db8
ozar98 <ozar007@gmail.com>
2020-12-03 00:42:28 +0500

    Update activity_science_matter_quiz_solids.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_matter_quiz_solids.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_matter_quiz_solids.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.r0adkll.slidr.Slidr;
+
 public class activity_science_matter_quiz_solids extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_science_matter_quiz_solids);
+        //sliding left to parent activity
+        Slidr.attach(this);
     }
 }
