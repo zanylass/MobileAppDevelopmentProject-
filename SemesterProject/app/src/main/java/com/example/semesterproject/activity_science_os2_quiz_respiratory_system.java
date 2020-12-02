a17e083fb715235a7cfb74fb26bf0bb4eda3dff2
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-02 15:56:32 +0600

    Create activity_science_os2_quiz_respiratory_system.java

---
+++ SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_os2_quiz_respiratory_system.java
@@ -0,0 +1,14 @@
+package com.example.semesterproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_science_os2_quiz_respiratory_system extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_science_os2_quiz_respiratory_system);
+    }
+}
