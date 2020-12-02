28741b19f25c2e01db2ce116fa6ffcb963570ce1
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-02 16:14:40 +0600

    Update activity_science_OS1_content.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_OS1_content.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_OS1_content.java
@@ -2,7 +2,9 @@ package com.example.semesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,5 +21,69 @@ public class activity_science_OS1_content extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_science__o_s1_content);
+
+        MS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMS();
+            }
+        });
+        DS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDS();
+            }
+        });
+        SS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSS();
+            }
+        });
+        btnM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizMS();
+            }
+        });
+        btnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizDS();
+            }
+        });
+        btnS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizSS();
+            }
+        });
+
     }
+
+    private void openMS() {
+        Intent intent = new Intent(this, activity_science_os1_muscular_system.class);
+        startActivity(intent);
+    }
+    private void openQuizMS() {
+        Intent intent = new Intent(this, activity_science_os1_quiz_muscular_system.class);
+        startActivity(intent);
+    }
+    private void openDS() {
+        Intent intent = new Intent(this, activity_science_os1_digestive_system.class);
+        startActivity(intent);
+    }
+    private void openQuizDS() {
+        Intent intent = new Intent(this, activity_science_os1_quiz_digestive_system.class);
+        startActivity(intent);
+    }
+    private void openSS() {
+        Intent intent = new Intent(this, activity_science_os1_skeletal_system.class);
+        startActivity(intent);
+    }
+    private void openQuizSS() {
+        Intent intent = new Intent(this, activity_science_os1_quiz_skeletal_system.class);
+        startActivity(intent);
+    }
+
 }
