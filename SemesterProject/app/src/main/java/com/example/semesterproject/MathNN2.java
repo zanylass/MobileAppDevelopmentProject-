8b1d93b2f84be4fc601fea6e97d5ce1a4483e265
ozar98 <ozar007@gmail.com>
2020-12-03 00:44:05 +0500

    Update MathNN2.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathNN2.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathNN2.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.r0adkll.slidr.Slidr;
+
 public class MathNN2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_n_n2);
+        //sliding left to parent activity
+        Slidr.attach(this);
     }
 }
