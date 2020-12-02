4ea11e361977858aac7838529d935473db7e6b49
ozar98 <ozar007@gmail.com>
2020-12-03 00:42:11 +0500

    Update activity_science_matter_content.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_matter_content.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_matter_content.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.r0adkll.slidr.Slidr;
+
 public class activity_science_matter_content extends AppCompatActivity {
 
     private TextView solids = (TextView) findViewById(R.id.textViewSolids);
@@ -21,6 +23,8 @@ public class activity_science_matter_content extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_science_matter_content);
+        //sliding left to parent activity
+        Slidr.attach(this);
 
         solids.setOnClickListener(new View.OnClickListener() {
             @Override
