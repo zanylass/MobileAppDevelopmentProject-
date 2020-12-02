5daddbcc21f3a0c564de2cddd8bba5f8cc45f491
ozar98 <ozar007@gmail.com>
2020-12-03 00:44:39 +0500

    Update MathPNx.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathPNx.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathPNx.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.r0adkll.slidr.Slidr;
+
 public class MathPNx extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_p_nx);
+        //sliding left to parent activity
+        Slidr.attach(this);
     }
 }
