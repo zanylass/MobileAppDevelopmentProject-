0e920960d6f028244c9dfba00f7ca3c528008465
ozar98 <ozar007@gmail.com>
2020-12-03 00:43:35 +0500

    Update MathAR1.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathAR1.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathAR1.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.r0adkll.slidr.Slidr;
+
 public class MathAR1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_a_r1);
+        //sliding left to parent activity
+        Slidr.attach(this);
     }
 }
