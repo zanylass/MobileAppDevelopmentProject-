ae763308e6bd0d5671d4d71854a298942d2da97f
ozar98 <ozar007@gmail.com>
2020-12-03 00:42:41 +0500

    Update activity_science_os1_digestive_system.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_os1_digestive_system.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_os1_digestive_system.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.r0adkll.slidr.Slidr;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -17,6 +18,8 @@ public class activity_science_os1_digestive_system extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_science_os1_digestive_system);
+        //sliding left to parent activity
+        Slidr.attach(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
