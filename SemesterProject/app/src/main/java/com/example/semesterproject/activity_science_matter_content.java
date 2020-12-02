ea140a0d6cde1d336113f2f983efd1db7c287135
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-02 15:26:58 +0600

    Update activity_science_matter_content.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_matter_content.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_matter_content.java
@@ -3,8 +3,17 @@ package com.example.semesterproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class activity_science_matter_content extends AppCompatActivity {
+
+    private TextView solids = (TextView) findViewById(R.id.textViewSolids);
+    private TextView liquids = (TextView) findViewById(R.id.textViewLiquids);
+    private TextView gases = (TextView) findViewById(R.id.textViewGases);
+    private Button btnSolids = (Button) findViewById(R.id.btnQuizSolids);
+    private Button btnLiquids = (Button) findViewById(R.id.btnQuizLiquids);
+    private Button btnGases = (Button) findViewById(R.id.btnQuizGases);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
