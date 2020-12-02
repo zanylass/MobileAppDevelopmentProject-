548de8608ea0bbdd0aa49035c5574b1024ad31de
ozar98 <ozar007@gmail.com>
2020-12-03 00:44:22 +0500

    Update MathPercentageContent.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathPercentageContent.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathPercentageContent.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.r0adkll.slidr.Slidr;
+
 public class MathPercentageContent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_percentage_content);
+        //sliding left to parent activity
+        Slidr.attach(this);
     }
 }
