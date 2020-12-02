eab3d4f2c1e61e1c2551fefa139c69746dc94c34
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-02 15:27:08 +0600

    Update activity_science_OS2_content.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_OS2_content.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_OS2_content.java
@@ -3,8 +3,19 @@ package com.example.semesterproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class activity_science_OS2_content extends AppCompatActivity {
+
+    private TextView NS = (TextView) findViewById(R.id.textViewNervous);
+    private TextView RS = (TextView) findViewById(R.id.textViewRespiratory);
+    private TextView CS = (TextView) findViewById(R.id.textViewCirculatory);
+    private TextView ES = (TextView) findViewById(R.id.textViewExcretory);
+    private Button btnN = (Button) findViewById(R.id.btnQuizNervous);
+    private Button btnR = (Button) findViewById(R.id.btnQuizRespiratory);
+    private Button btnC = (Button) findViewById(R.id.btnQuizCirculatory);
+    private Button btnE = (Button) findViewById(R.id.btnQuizExcretory);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
