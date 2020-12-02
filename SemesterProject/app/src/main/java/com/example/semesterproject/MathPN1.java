e3fe7b3c3b99e83d36a4842cfcd85e604f6af82d
ozar98 <ozar007@gmail.com>
2020-12-03 00:44:26 +0500

    Update MathPN1.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/MathPN1.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/MathPN1.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.r0adkll.slidr.Slidr;
+
 public class MathPN1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_p_n1);
+        //sliding left to parent activity
+        Slidr.attach(this);
     }
 }
