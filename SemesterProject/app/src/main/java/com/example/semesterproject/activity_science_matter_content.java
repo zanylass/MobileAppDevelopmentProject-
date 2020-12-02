ae5f75c22f928240091e9418e1a0cbb0f73c3887
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-02 15:55:23 +0600

    Update activity_science_matter_content.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_matter_content.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_matter_content.java
@@ -2,7 +2,9 @@ package com.example.semesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,5 +21,71 @@ public class activity_science_matter_content extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_science_matter_content);
+        solids.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMatterSolids();
+            }
+        });
+
+        btnSolids.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizSolids();
+            }
+        });
+
+        liquids.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMatterLiquids();
+            }
+        });
+
+        btnLiquids.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizLiquids();
+            }
+        });
+
+        gases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMatterGases();
+            }
+        });
+
+        btnGases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizGases();
+            }
+        });
+    }
+
+    private void openMatterSolids() {
+        Intent intent = new Intent(this, activity_science_matter_solids.class);
+        startActivity(intent);
+    }
+    private void openQuizSolids() {
+        Intent intent = new Intent(this, activity_science_matter_quiz_solids.class);
+        startActivity(intent);
+    }
+    private void openMatterLiquids() {
+        Intent intent = new Intent(this, activity_science_matter_liquids.class);
+        startActivity(intent);
+    }
+    private void openQuizLiquids() {
+        Intent intent = new Intent(this, activity_science_matter_quiz_liquids.class);
+        startActivity(intent);
+    }
+    private void openMatterGases() {
+        Intent intent = new Intent(this, activity_science_matter_gases.class);
+        startActivity(intent);
+    }
+    private void openQuizGases() {
+        Intent intent = new Intent(this, activity_science_matter_quiz_gases.class);
+        startActivity(intent);
     }
 }
