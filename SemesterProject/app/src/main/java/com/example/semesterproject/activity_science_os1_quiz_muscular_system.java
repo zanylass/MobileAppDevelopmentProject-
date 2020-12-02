1c660692d4c865af82a8f4e73aeeff5ff921736b
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-02 15:56:00 +0600

    Create activity_science_os1_quiz_muscular_system.java

---
+++ SemesterProject/app/src/main/java/com/example/semesterproject/activity_science_os1_quiz_muscular_system.java
@@ -0,0 +1,14 @@
+package com.example.semesterproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_science_os1_quiz_muscular_system extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_science_os1_quiz_muscular_system);
+    }
+}
