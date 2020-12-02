9139da3b08f55c02e232669bb37bb67d3e55291f
ozar98 <ozar007@gmail.com>
2020-12-03 00:45:26 +0500

    Update RatioContentPage.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/RatioContentPage.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/RatioContentPage.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.r0adkll.slidr.Slidr;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
@@ -14,6 +16,8 @@ public class RatioContentPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ratio_content_page);
+        //sliding left to parent activity
+        Slidr.attach(this);
         
     }
 }
