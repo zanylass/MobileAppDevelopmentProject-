55c144e427038f700052855a7543e41843deeeda
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-03 20:42:25 +0600

    Update ScienceMatterContent.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/ScienceMatterContent.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/ScienceMatterContent.java
@@ -1,4 +1,18 @@
 package com.example.semesterproject;
 
-public class ScienceMatterContent {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.r0adkll.slidr.Slidr;
+
+public class ScienceMatterContent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_science_matter_content);
+        //sliding left to parent activity
+        Slidr.attach(this);
+    }
 }
