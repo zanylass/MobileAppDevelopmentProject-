47b299e8ee149b3ff0a80c931630061948b2e900
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-03 22:01:59 +0600

    Update ScienceOS1Content.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/ScienceOS1Content.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/ScienceOS1Content.java
@@ -2,7 +2,9 @@ package com.example.semesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,15 +13,8 @@ import com.r0adkll.slidr.Slidr;
 
 public class ScienceOS1content extends AppCompatActivity {
 
-    private TextView NS = findViewById(R.id.textViewNervousSystem);
-    private TextView RS = findViewById(R.id.textViewRespiratorySystem);
-    private TextView ES = findViewById(R.id.textViewExcretorySystem);
-    private TextView CS = findViewById(R.id.textViewCirculatorySystem);
-    private Button btnNS = findViewById(R.id.button);
-    private Button btnRS = findViewById(R.id.button2);
-    private Button btnES = findViewById(R.id.button3);
-    private Button btnCS = findViewById(R.id.button4);
-
+    public TextView NS = findViewById(R.id.textViewNervousSystem);
+    public TextView RS = findViewById(R.id.textViewRespiratorySystem);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +22,29 @@ public class ScienceOS1content extends AppCompatActivity {
         setContentView(R.layout.activity_science_o_s1content);
         //sliding left to parent activity
         Slidr.attach(this);
+        NS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNSContent();
+            }
+        });
 
+        RS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRSContent();
+            }
+        });
 
+    }
+
+    private void openNSContent() {
+        Intent intent = new Intent(this, ScienceOS1NervousSystem.class);
+        startActivity(intent);
+    }
+
+    private void openRSContent() {
+        Intent intent = new Intent(this, ScienceOS1RespiratorySystem.class);
+        startActivity(intent);
     }
 }
