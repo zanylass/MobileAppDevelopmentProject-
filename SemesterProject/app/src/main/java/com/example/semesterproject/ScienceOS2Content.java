182e051a1ff36b8f63f2997b5e509b07d74d2d88
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-03 20:42:34 +0600

    Update ScienceOS2Content.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/ScienceOS2Content.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/ScienceOS2Content.java
@@ -1,4 +1,18 @@
 package com.example.semesterproject;
 
-public class ScienceOS2Content {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.r0adkll.slidr.Slidr;
+
+public class ScienceOS2content extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_science_o_s2content);
+        //sliding left to parent activity
+        Slidr.attach(this);
+    }
 }
