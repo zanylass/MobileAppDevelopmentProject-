eca9a812119d576f3c97eecb1cc66d747d0bd4e0
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-03 21:29:20 +0600

    Update ScienceOS1Content.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/ScienceOS1Content.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/ScienceOS1Content.java
@@ -3,10 +3,23 @@ package com.example.semesterproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.r0adkll.slidr.Slidr;
 
 public class ScienceOS1content extends AppCompatActivity {
+
+    private TextView NS = findViewById(R.id.textViewNervousSystem);
+    private TextView RS = findViewById(R.id.textViewRespiratorySystem);
+    private TextView ES = findViewById(R.id.textViewExcretorySystem);
+    private TextView CS = findViewById(R.id.textViewCirculatorySystem);
+    private Button btnNS = findViewById(R.id.button);
+    private Button btnRS = findViewById(R.id.button2);
+    private Button btnES = findViewById(R.id.button3);
+    private Button btnCS = findViewById(R.id.button4);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +27,7 @@ public class ScienceOS1content extends AppCompatActivity {
         setContentView(R.layout.activity_science_o_s1content);
         //sliding left to parent activity
         Slidr.attach(this);
+
+        
     }
 }
