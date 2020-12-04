48ad0b3b60664f9835d2c89bf74aeeead266edc8
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-04 21:14:35 +0600

    Create ScienceMatterChanges1.java

---
+++ SemesterProject/app/src/main/java/com/example/semesterproject/ScienceMatterChanges1.java
@@ -0,0 +1,17 @@
+package com.example.semesterproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.r0adkll.slidr.Slidr;
+
+public class ScienceMatterChanges1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_science_matter_changes1);
+        Slidr.attach(this);
+    }
+}
