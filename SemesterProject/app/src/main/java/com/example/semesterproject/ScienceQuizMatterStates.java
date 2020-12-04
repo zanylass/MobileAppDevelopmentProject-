1ec4b4e4d9d359c6efa4997035dde514b8efdcdb
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-04 21:15:13 +0600

    Create ScienceQuizMatterStates.java

---
+++ SemesterProject/app/src/main/java/com/example/semesterproject/ScienceQuizMatterStates.java
@@ -0,0 +1,14 @@
+package com.example.semesterproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScienceQuizMatterStates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_science_quiz_matter_states);
+    }
+}
