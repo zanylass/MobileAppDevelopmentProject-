13b762241aaa78c05d8cce14890686d5943cc528
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-04 21:27:18 +0600

    Update ScienceOS1Content.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/ScienceOS1Content.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/ScienceOS1Content.java
@@ -5,49 +5,79 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.r0adkll.slidr.Slidr;
 
-public class ScienceOS1Content extends AppCompatActivity {
+public class ScienceOs1Content extends AppCompatActivity {
 
-    public TextView NS ;
-    public TextView RS;
+    private TextView NS;
+    private TextView RS;
+    private TextView ES;
+    private TextView CS;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_science_o_s1content);
+        setContentView(R.layout.activity_science_os1_content);
         //sliding left to parent activity
         Slidr.attach(this);
-        NS = (TextView) findViewById(R.id.textViewNervousSystem);
-        RS = (TextView) findViewById(R.id.textViewRespiratorySystem);
+
+        NS = (TextView) findViewById(R.id.textViewNervous);
+        RS = (TextView) findViewById(R.id.textViewRespiratory);
+        ES = (TextView) findViewById(R.id.textViewExcretory);
+        CS = (TextView) findViewById(R.id.textViewCirculatory);
 
         NS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openNSContent();
+                openNS();
             }
         });
 
         RS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openRSContent();
+                openRS();
+            }
+        });
+
+        ES.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openES();
+            }
+        });
+
+        CS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCS();
             }
         });
 
     }
 
-    private void openNSContent() {
-        Intent intent = new Intent(this, ScienceOS1NervousSystem.class);
+    private void openNS() {
+        Intent intent = new Intent(this, ScienceOs1Nervoussystem.class);
         startActivity(intent);
     }
 
-    private void openRSContent() {
-        Intent intent = new Intent(this, ScienceOS1RespiratorySystem.class);
+    private void openRS() {
+        Intent intent = new Intent(this, ScienceOs1Respiratorysystem.class);
         startActivity(intent);
     }
+
+    private void openES() {
+        Intent intent = new Intent(this, ScienceOs1Excretorysystem.class);
+        startActivity(intent);
+    }
+
+    private void openCS() {
+        Intent intent = new Intent(this, ScienceOs1Circulatorysystem.class);
+        startActivity(intent);
+    }
+
+
 }
