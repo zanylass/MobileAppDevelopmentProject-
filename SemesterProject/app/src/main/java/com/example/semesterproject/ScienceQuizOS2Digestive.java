21ac2524bd23c21a18466cc3b6bc71b1eb4bd4c5
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-08 01:53:33 +0600

    Create ScienceQuizOS2Digestive.java

---
+++ SemesterProject/app/src/main/java/com/example/semesterproject/ScienceQuizOS2Digestive.java
@@ -0,0 +1,261 @@
+package com.example.semesterproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.r0adkll.slidr.Slidr;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+
+public class ScienceQuizOS2Digestive extends AppCompatActivity {
+
+    public static final String EXTRA_SCORE="extraScore";
+    private static final long COUNTDOWN_IN_MILLIS=30000;
+
+    private static final String KEY_SCORE="keyScore";
+    private static final String KEY_QUESTION_COUNT="keyQuestionCount";
+    private static final String KEY_MILLIS_LEFT="keyMillisLeft";
+    private static final String KEY_ANSWERED="keyAnswered";
+    private static final String KEY_QUESTION_LIST="keyQuestionList";
+
+    private Button checkButton;
+    private TextView textViewQuestion;
+    private TextView textViewScore;
+    private TextView textViewQuestionCount;
+    private TextView textViewCountDown;
+    private RadioGroup rbGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private RadioButton rb4;
+
+    private ColorStateList textColorDefaultRb;
+    private ColorStateList textColorDefaultCd;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
+
+    private int questionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+
+    private int score;
+    private boolean answered;
+
+    private long backPressedTime;
+
+    private ArrayList<Question> questionList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_science_quiz_matter_states);
+        //sliding left to parent activity
+        Slidr.attach(this);
+        checkButton=(Button)findViewById(R.id.buttonCheck2);
+        textViewQuestion=(TextView)findViewById(R.id.text_view_question2);
+        textViewScore=(TextView)findViewById(R.id.text_view_score2);
+        textViewQuestionCount=(TextView)findViewById(R.id.text_view_question_count2);
+        textViewCountDown=(TextView)findViewById(R.id.text_view_countdown2);
+        rbGroup=(RadioGroup)findViewById(R.id.radio_group2);
+        rb1=(RadioButton)findViewById(R.id.radio_buttonA);
+        rb2=(RadioButton)findViewById(R.id.radio_buttonB);
+        rb3=(RadioButton)findViewById(R.id.radio_buttonC);
+        rb4=(RadioButton)findViewById(R.id.radio_buttonD);
+
+        textColorDefaultRb=rb1.getTextColors();
+        textColorDefaultCd=textViewCountDown.getTextColors();
+
+        if(savedInstanceState==null){
+            QuizDbHelperOS2A dbHelper=new QuizDbHelperOS2A(this);
+            questionList= dbHelper.getAllQuestions();
+            questionCountTotal=questionList.size();
+            Collections.shuffle(questionList);
+
+            showNextQuestion();
+        }else {
+            questionList=savedInstanceState.getParcelableArrayList(KEY_QUESTION_LIST);
+            questionCountTotal=questionList.size();
+            questionCounter=savedInstanceState.getInt(KEY_QUESTION_COUNT);
+            currentQuestion=questionList.get(questionCounter-1);
+            score=savedInstanceState.getInt(KEY_SCORE);
+            timeLeftInMillis=savedInstanceState.getLong(KEY_MILLIS_LEFT);
+            answered=savedInstanceState.getBoolean(KEY_ANSWERED);
+
+            if(!answered){
+                startCountDown();
+            }else{
+                updateCountDownText();
+                showSolution();
+            }
+        }
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!answered){
+                    if(rb1.isChecked()|| rb2.isChecked() || rb3.isChecked() || rb4.isChecked()){
+                        checkAnswer();
+                    }else{
+                        Toast.makeText(ScienceQuizOS2Digestive.this, "Please select an answer", Toast.LENGTH_SHORT).show();
+                    }
+                } else{
+                    showNextQuestion();
+                }
+                //openQuizPage2();
+            }
+        });
+    }
+    private void showNextQuestion(){
+        rb1.setTextColor(textColorDefaultRb);
+        rb2.setTextColor(textColorDefaultRb);
+        rb3.setTextColor(textColorDefaultRb);
+        rb4.setTextColor(textColorDefaultRb);
+        rbGroup.clearCheck();
+
+        if (questionCounter<questionCountTotal){
+            currentQuestion=questionList.get(questionCounter);
+            textViewQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+            rb3.setText(currentQuestion.getOption3());
+            rb4.setText(currentQuestion.getOption4());
+
+            questionCounter++;
+            textViewQuestionCount.setText("Question: "+questionCounter+"/"+questionCountTotal);
+            answered=false;
+            checkButton.setText("Confirm");
+
+            timeLeftInMillis=COUNTDOWN_IN_MILLIS;
+            startCountDown();
+        }else{
+            finishQuiz();
+        }
+    }
+
+    private void startCountDown(){
+        countDownTimer=new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis=millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timeLeftInMillis=0;
+                updateCountDownText();
+                checkAnswer();
+
+            }
+        }.start();
+    }
+
+    private void updateCountDownText(){
+        int minutes=(int)(timeLeftInMillis/1000)/60;
+        int seconds=(int)(timeLeftInMillis/1000)%60;
+
+        String timeFormatted=String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
+        textViewCountDown.setText(timeFormatted);
+        if (timeLeftInMillis<10000){
+            textViewCountDown.setTextColor(Color.RED);
+        }else{
+            textViewCountDown.setTextColor(textColorDefaultCd);
+        }
+
+    }
+
+    private void checkAnswer(){
+        answered=true;
+        countDownTimer.cancel();
+        RadioButton rbSelected=findViewById(rbGroup.getCheckedRadioButtonId());
+        int answerNr=rbGroup.indexOfChild(rbSelected)+1;
+        if (answerNr==currentQuestion.getAnswerNr()){
+            score++;
+            textViewScore.setText("Score: "+ score);
+        }
+        showSolution();
+    }
+
+    private void showSolution(){
+        rb1.setTextColor(Color.RED);
+        rb2.setTextColor(Color.RED);
+        rb3.setTextColor(Color.RED);
+        rb4.setTextColor(Color.RED);
+
+        switch (currentQuestion.getAnswerNr()){
+            case 1:
+                rb1.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 1 is correct");
+                break;
+            case 2:
+                rb2.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 2 is correct");
+                break;
+            case 3:
+                rb3.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 3 is correct");
+                break;
+            case 4:
+                rb4.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer 4 is correct");
+                break;
+        }
+        if (questionCounter<questionCountTotal){
+            checkButton.setText("Next");
+        }else{
+            checkButton.setText("Finish");
+        }
+    }
+
+
+    private void finishQuiz(){
+        Intent resultIntent= new Intent();
+        resultIntent.putExtra(EXTRA_SCORE, score);
+        setResult(RESULT_OK, resultIntent);
+        finish();
+
+    }
+    @Override
+    //accidental leaving-saving the progress
+    public void onBackPressed(){
+        if (backPressedTime+2000> System.currentTimeMillis()){
+            finishQuiz();
+        }else{
+            Toast.makeText(this, "Press back again to finish", Toast.LENGTH_SHORT).show();
+        }
+        backPressedTime=System.currentTimeMillis();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer!=null){
+            countDownTimer.cancel();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_SCORE, score);
+        outState.putInt(KEY_QUESTION_COUNT, questionCounter);
+        outState.putLong(KEY_MILLIS_LEFT, timeLeftInMillis);
+        outState.putBoolean(KEY_ANSWERED, answered);
+        outState.putParcelableArrayList(KEY_QUESTION_LIST, questionList);
+    }
+
+}
