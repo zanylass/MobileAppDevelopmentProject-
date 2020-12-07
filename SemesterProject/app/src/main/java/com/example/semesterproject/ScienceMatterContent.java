4609b9c11c6e876b5e75736489294f367fc6b7d7
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-08 01:55:22 +0600

    Update ScienceMatterContent.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/ScienceMatterContent.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/ScienceMatterContent.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.r0adkll.slidr.Slidr;
@@ -14,6 +15,9 @@ public class ScienceMatterContent extends AppCompatActivity {
     private TextView SoM;
     private TextView CM1;
     private TextView CM2;
+    private Button btnQuiz1;
+    private Button btnQuiz2;
+    private Button btnQuiz3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,9 @@ public class ScienceMatterContent extends AppCompatActivity {
         SoM = (TextView) findViewById(R.id.textViewMatterStates);
         CM1 = (TextView) findViewById(R.id.textViewMatterChanges1);
         CM2 = (TextView) findViewById(R.id.textViewMatterChanges2);
+        btnQuiz3 = (Button) findViewById(R.id.btnQuizMatterStates);
+        btnQuiz1 = (Button) findViewById(R.id.btnQuizChanges1);
+        btnQuiz2 = (Button) findViewById(R.id.btnQuizChanges2);
 
         SoM.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +54,27 @@ public class ScienceMatterContent extends AppCompatActivity {
             }
         });
 
+        btnQuiz3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizSoM();
+            }
+        });
+
+        btnQuiz1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizCM1();
+            }
+        });
+
+        btnQuiz2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizCM2();
+            }
+        });
+
     }
 
     private void openSoM() {
@@ -64,5 +92,18 @@ public class ScienceMatterContent extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void openQuizCM1(){
+        Intent intent = new Intent(this, ScienceQuizChangesMatter1.class);
+        startActivity(intent);
+    }
 
+    private void openQuizCM2(){
+        Intent intent = new Intent(this, ScienceQuizChangesMatter2.class);
+        startActivity(intent);
+    }
+
+    private void openQuizSoM(){
+        Intent intent = new Intent(this, ScienceQuizMatterStates.class);
+        startActivity(intent);
+    }
 }
