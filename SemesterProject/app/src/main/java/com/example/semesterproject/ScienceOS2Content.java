5a25de33a9f1678d7e722397a959ffed12ea3334
zanylass <57338344+zanylass@users.noreply.github.com>
2020-12-08 01:55:12 +0600

    Update ScienceOS2Content.java

--- SemesterProject/app/src/main/java/com/example/semesterproject/ScienceOS2Content.java
+++ SemesterProject/app/src/main/java/com/example/semesterproject/ScienceOS2Content.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.r0adkll.slidr.Slidr;
@@ -14,6 +15,10 @@ public class ScienceOs2Content extends AppCompatActivity {
     private TextView MS;
     private TextView SS;
     private TextView DS;
+    private Button btnQuiz1;
+    private Button btnQuiz2;
+    private Button btnQuiz3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,9 @@ public class ScienceOs2Content extends AppCompatActivity {
         MS = (TextView) findViewById(R.id.textViewMuscularsystem);
         SS = (TextView) findViewById(R.id.textViewSkeletalsystem);
         DS = (TextView) findViewById(R.id.textViewDigestivesystem);
+        btnQuiz1 = (Button) findViewById(R.id.btnQuizMuscular);
+        btnQuiz2 = (Button) findViewById(R.id.btnQuizSkeletal);
+        btnQuiz3 = (Button) findViewById(R.id.btnQuizDigestive);
 
         MS.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,6 +53,25 @@ public class ScienceOs2Content extends AppCompatActivity {
             }
         });
 
+        btnQuiz1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizMS();
+            }
+        });
+        btnQuiz2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizSS();
+            }
+        });
+        btnQuiz3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizDS();
+            }
+        });
+
     }
 
     private void openMS(){
@@ -59,6 +86,21 @@ public class ScienceOs2Content extends AppCompatActivity {
 
     private void openDS() {
         Intent intent = new Intent(this, ScienceOs2Digestivesystem.class);
+        startActivity(intent);
+    }
+
+    private void openQuizMS(){
+        Intent intent = new Intent(this, ScienceQuizOS2Muscular.class);
+        startActivity(intent);
+    }
+
+    private void openQuizSS(){
+        Intent intent = new Intent(this, ScienceQuizOS2Skeletal.class);
+        startActivity(intent);
+    }
+
+    private void openQuizDS() {
+        Intent intent = new Intent(this, ScienceQuizOS2Digestive.class);
         startActivity(intent);
     }
 }
